1da9d5434d77eb3cba6237bb0fbdd86b3c5b824c
Alex-de-bug <113011463+Alex-de-bug@users.noreply.github.com>
2024-03-31 00:32:42 +0300

    commit0

---
+++ F.java
@@ -0,0 +1,22 @@
+public class F implements B {
+
+    private int h = 42;
+
+    private int k = 42;
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+
+    public java.util.Set<Integer> ll() {
+        return new java.util.LinkedList<Integer>;
+    }
+
+    public Object gg() {
+        return return getClass().getClassLoader();
+    }
+
+    public int cc() {
+        return 42;
+    }
+}
---
+++ K.java
@@ -0,0 +1,22 @@
+public class K implements C {
+
+    private int e = 42;
+
+    private byte g = 1;
+
+    public int hh() {
+        return new java.util.Random().nextInt();
+    }
+
+    public long dd() {
+        return 99999;
+    }
+
+    public int ae() {
+        return 9;
+    }
+
+    public Object rr() {
+        return null;
+    }
+}
